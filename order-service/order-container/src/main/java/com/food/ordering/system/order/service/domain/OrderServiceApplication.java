724e0d295cf249999b439671a02bca7a1a021fa6
Aryan Thodupunuri <aryan20544@gmail.com>
2025-05-08 15:56:03 -0400

    Add base Spring Boot setup for order, payment, restaurant, and customer services

--- order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
+++ order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
@@ -1,9 +1,9 @@
 package com.food.ordering.system.order.service.domain;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @EnableJpaRepositories(basePackages = { "com.food.ordering.system.order.service.dataaccess", "com.food.ordering.system.dataaccess" })
 @EntityScan(basePackages = { "com.food.ordering.system.order.service.dataaccess", "com.food.ordering.system.dataaccess"})
