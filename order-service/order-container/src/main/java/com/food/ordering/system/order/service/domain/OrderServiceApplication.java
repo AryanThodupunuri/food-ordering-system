1c426480cdc272962d37528332a65ffffee642ce
Aryan Thodupunuri <aryan20544@gmail.com>
2025-07-10 15:42:05 -0400

    Add unit tests for Order domain model and service logic

--- order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
+++ order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
@@ -3,10 +3,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-
-@EnableJpaRepositories(basePackages = { "com.food.ordering.system.order.service.dataaccess", "com.food.ordering.system.dataaccess" })
-@EntityScan(basePackages = { "com.food.ordering.system.order.service.dataaccess", "com.food.ordering.system.dataaccess"})
+/** 
+* Main entry point for the Order Service application
+ * Bootstrap for the Spring Boot application, configures component scanning,
+ * enables JPA repositories, and specifies entity scanning for data access layers.
+ */
+@EnableJpaRepositories(basePackages = { 
+    "com.food.ordering.system.order.service.dataaccess", 
+    "com.food.ordering.system.dataaccess" 
+})
+// Enables JPA repository support for the specified packages where repository interfaces are defined
+@EntityScan(basePackages = { 
+    "com.food.ordering.system.order.service.dataaccess",
+    "com.food.ordering.system.dataaccess"
+})
+// Scans the specified packages for JPA entity classes
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
 public class OrderServiceApplication {
     public static void main(String[] args) {
